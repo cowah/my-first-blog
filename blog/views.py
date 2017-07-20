056d4fb986a892acf70568a20083928f076c26e7
cwow <colleen.wahl@girlswhocode.com>
2017-07-19 21:18:43 -0700

    adding a new template extension.

--- blog/views.py
+++ blog/views.py
@@ -7,4 +7,5 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
-    Post.objects.get_object_or_404(pk=pk)
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
